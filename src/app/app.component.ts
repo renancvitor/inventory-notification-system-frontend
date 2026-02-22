767fab247f0650a80b3ab2b583e18dfc9e9ac60f
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-02-22 17:02:49 -0300

    feat(component): adds footer component (#11)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [ ]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
