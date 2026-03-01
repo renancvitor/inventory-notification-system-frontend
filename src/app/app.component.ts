1f63770048a7c5f48c148b4a01fcd259abd4227c
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-03-01 11:41:39 -0300

    feat(component): adds button component and create global styles for S… (#16)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [ ]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, FooterComponent],
+  standalone: true,
+  imports: [LoginComponent, FooterComponent, ButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('inventory-notification-system-frontend');
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [MatButtonModule, CommonModule],
+  templateUrl: './button.html',
+  styleUrls: ['./button.scss'],
+})
+export class ButtonComponent {
+
+  @Input() customClass: string = '';
+  @Input() label: string = '';
+  @Input() disabled: boolean = false;
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+
+}
