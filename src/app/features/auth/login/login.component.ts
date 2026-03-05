22d32b3d7fd8ed264d3518d9ebd1bd64c70140e3
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-05 20:56:38 -0300

    feat(auth): login form layout using Angular Material

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../core/auth/auth.service';
 import { Router } from '@angular/router';
-import { email } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, ButtonComponent],
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
 })
