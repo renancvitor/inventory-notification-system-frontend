b35151ead96a55d0977566dcb916609e46718923
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-03-05 21:28:01 -0300

    feat(auth): implement login form validation (#25)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [ ]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -5,19 +5,27 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatCardModule, MatInputModule, ButtonComponent],
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, ButtonComponent, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
 })
 export class LoginComponent {
+  private formBuilder = new FormBuilder();
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
   constructor(private authService: AuthService, private router: Router) {}
 
-  loginTest() {
+  login() {
     this.authService.login({
       email: 'admin@sistema.com',
       password: '123456',
