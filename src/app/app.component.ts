b63e97952766d4ded44a2d3a0d6500f8f505332e
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-08 20:27:26 -0300

    feat(header): add HeaderComponent with navigation and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { AuthService } from './core/auth/auth.service';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [FooterComponent, RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
+
   protected readonly title = signal('inventory-notification-system-frontend');
+
+  constructor(public authService: AuthService) {}
 }
--- src/app/core/errors/error.service.ts
+++ src/app/core/errors/error.service.ts
@@ -10,15 +10,11 @@ export class ErrorService {
     constructor(private snackBar: MatSnackBar) {}
 
     showError(message: string) {
-        const ref = this.snackBar.openFromComponent(ErrorSnackbarComponent, {
+        this.snackBar.openFromComponent(ErrorSnackbarComponent, {
             data: { message },
             duration: 5000,
             horizontalPosition: 'center',
             verticalPosition: 'top'
-        });
-
-        ref.afterDismissed().subscribe(() => {
-            window.location.reload();
         });
     }
 }
