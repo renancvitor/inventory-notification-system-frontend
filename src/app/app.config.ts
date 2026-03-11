1315db34a4b376d212b38fe49f74576f650ec108
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-10 21:56:59 -0300

    feat(person): create person service integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { errorInterceptor } from './core/errors/error.interceptor';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/api/auth-interceptor';
+import { authInterceptor } from './core/api/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
