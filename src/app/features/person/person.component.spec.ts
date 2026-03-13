b4fa2a6c95930f94df9cdaeb9e957532a7c69098
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-12 21:44:28 -0300

    feat(person): add person component base

--- src/app/features/person/person.component.spec.ts
+++ src/app/features/person/person.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { PersonComponent } from './person.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('PersonComponent', () => {
+  let component: PersonComponent;
+  let fixture: ComponentFixture<PersonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [PersonComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
