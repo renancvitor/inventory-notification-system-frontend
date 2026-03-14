f16711e2049f2ecb18335cd36c4e43548bd59127
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-03-13 22:50:19 -0300

    feat(person): integrate person list with backend (#44)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [ ]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

--- src/app/features/person/person.component.ts
+++ src/app/features/person/person.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
+import { PersonService } from './person.service';
 
 @Component({
   selector: 'app-person.component',
@@ -18,5 +20,29 @@ export class PersonComponent {
   displayedColumns: string[] = ['name', 'email', 'active', 'actions'];
 
   dataSource: any[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private personService: PersonService) {}
+
+  ngAfterViewInit() {
+    this.loadPersons();
+
+    this.paginator.page.subscribe(() => {
+      this.loadPersons();
+    });
+  }
+
+  loadPersons() {
+
+    this.personService.list({
+      page: this.paginator?.pageIndex ?? 0,
+      size: this.paginator?.pageSize ?? 10
+    })
+    .subscribe((response: any) => {
+      this.dataSource = response.content;
+    });
+
+  }
 
 }
