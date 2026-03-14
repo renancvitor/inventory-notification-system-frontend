0e93fe0d4cd79c182c3cb2067334c20c0951ad30
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-14 20:37:53 -0300

    feat(person): register person route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth-guard';
 import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
+import { PersonComponent } from './features/person/person.component';
 
 export const routes: Routes = [
 
@@ -14,6 +15,12 @@ export const routes: Routes = [
         path: '',
         canActivate: [authGuard],
         component: HomeComponent
+    },
+
+    {
+        path: 'person',
+        canActivate: [authGuard],
+        component: PersonComponent
     },
 
     {
