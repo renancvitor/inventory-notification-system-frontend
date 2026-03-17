5c5b2edbd9fab3ef06f0f13df01a6740b5e6fe8d
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-03-16 22:03:08 -0300

    feat(person): implements toast component (#53)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [x]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

---
+++ src/app/shared/components/toast/toast.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, HostListener } from '@angular/core';
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-toast',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss'],
+})
+export class ToastComponent {
+
+  constructor(
+    @Inject(MAT_SNACK_BAR_DATA) public data: any,
+    private snackBarRef: MatSnackBarRef<ToastComponent>
+  ) {}
+
+  @HostListener('document:keydown.escape') handleEscape() {
+    this.snackBarRef.dismiss();
+  }
+
+  close() {
+    this.snackBarRef.dismiss();
+  }
+
+  action(action: string) {
+    this.snackBarRef.dismissWithAction();
+    this.data.onAction(action);
+  }
+
+}
