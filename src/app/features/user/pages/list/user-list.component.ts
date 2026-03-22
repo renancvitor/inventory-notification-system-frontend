b25939de910bea190d0a631d5251628050b30460
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-21 23:14:26 -0300

    feat(user): configure filter button

--- src/app/features/user/pages/list/user-list.component.ts
+++ src/app/features/user/pages/list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +8,8 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRadioGroup } from '@angular/material/radio';
 
 import { UserService } from '../../user.service';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
@@ -16,7 +19,8 @@ import { FilterPanelComponent } from '../../../../shared/components/filter-panel
 @Component({
   selector: 'app-user-list.component',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule, MatPaginatorModule, MatButtonModule, ButtonComponent, SearchFieldComponent, MatMenuModule, FilterPanelComponent],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatPaginatorModule, MatButtonModule, ButtonComponent, SearchFieldComponent, MatMenuModule, 
+    FilterPanelComponent, MatRadioModule, MatRadioGroup, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
@@ -40,9 +44,30 @@ export class UserListComponent {
 
   totalUsers = 0;
 
+  filters = {
+    status: 'all'
+  };
+
+  applyFilters() {
+    this.paginator.pageIndex = 0;
+    this.loadUsers();
+  }
+
+  clearFilters() {
+    this.filters.status = 'all';
+    this.paginator.pageIndex = 0;
+    this.loadUsers();
+  }
+
   loadUsers() {
 
+    const active =
+      this.filters.status === 'all'
+        ? undefined
+        : this.filters.status === 'active';
+
     this.userService.list({
+      active,
       page: this.paginator?.pageIndex ?? 0,
       size: this.paginator?.pageSize ?? 10
     })
