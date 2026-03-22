66711e57a8307b8f9eb5601942d71cf15f969025
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-21 22:13:59 -0300

    feat(user): integrate UserComponent with backend

--- src/app/features/user/pages/list/user-list.component.ts
+++ src/app/features/user/pages/list/user-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
+
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-user-list.component',
@@ -17,5 +19,29 @@ export class UserListComponent {
   displayedColumns: string[] = ['name', 'email', 'userType', 'active', 'actions'];
 
   dataSource: any[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private userService: UserService) {}
+  
+  ngAfterViewInit() {
+    this.loadUsers();
+
+    this.paginator.page.subscribe(() => {
+      this.loadUsers();
+    });
+  }
+
+  loadUsers() {
+
+    this.userService.list({
+      page: this.paginator?.pageIndex ?? 0,
+      size: this.paginator?.pageSize ?? 10
+    })
+    .subscribe((response: any) => {
+      this.dataSource = response.content;
+    });
+    
+  }
 
 }
