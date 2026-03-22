9df91a32cd87d65692dad71530bd3c80c300a05e
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-21 23:00:23 -0300

    feat(user): create router registry

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth.guard';
+
 import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
+
 import { PersonComponent } from './features/person/pages/list/person-list.component';
 import { PersonCreateComponent } from './features/person/pages/create/person-create.component';
 import { PersonEditComponent } from './features/person/pages/edit/person-edit.component';
+
+import { UserListComponent } from './features/user/pages/list/user-list.component';
 
 export const routes: Routes = [
 
@@ -34,6 +38,12 @@ export const routes: Routes = [
     {
         path: 'person/edit/:id',
         component: PersonEditComponent
+    },
+
+    {
+        path: 'users',
+        canActivate: [authGuard],
+        component: UserListComponent
     },
 
     {
