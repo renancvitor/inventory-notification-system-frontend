23fbe63bee38bdddd091f53d227e53da6367c9a4
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-03-28 11:25:36 -0300

    feat(order): implements OrderService (#107)
    ### Checklist
    
    - [x]  Esta PR está vinculada a uma Issue
    - [x]  O escopo da Issue foi atendido
    - [x]  Código segue o padrão do projeto
    - [x]  Testes foram adicionados ou atualizados (se necessário)
    - [x]  Nenhum commit desnecessário incluído
    - [x]  Não há código comentado ou temporário
    - [ ]  Documentação foi atualizada (se necessário)
    - [x]  Não há breaking changes não documentadas

---
+++ src/app/features/order/order.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface OrderListParams {
+  page?: number;
+  size?: number;
+  search?: string;
+  orderStatusId?: number;
+  requestedBy?: number;
+  approvedBy?: number;
+  rejectedBy?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  totalValue?: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+
+  private readonly apiUrl = `${environment.apiUrl}/orders`;
+
+  constructor(private http: HttpClient) {}
+
+  list(params?: OrderListParams) {
+    return this.http.get(this.apiUrl, {
+      params: {
+        ...(params?.page !== undefined && { page: params.page }),
+        ...(params?.size !== undefined && { size: params.size }),
+        ...(params?.search !== undefined && { search: params.search }),
+        ...(params?.orderStatusId !== undefined && { orderStatusId: params.orderStatusId }),
+        ...(params?.requestedBy !== undefined && { requestedBy: params.requestedBy }),
+        ...(params?.approvedBy !== undefined && { approvedBy: params.approvedBy }),
+        ...(params?.rejectedBy !== undefined && { rejectedBy: params.rejectedBy }),
+        ...(params?.createdAt !== undefined && { createdAt: params.createdAt }),
+        ...(params?.updatedAt !== undefined && { updatedAt: params.updatedAt }),
+        ...(params?.totalValue !== undefined && { totalValue: params.totalValue })
+      },
+      withCredentials: true
+    });
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data, { 
+      withCredentials: true
+    });
+  }
+
+  update(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data, {
+      withCredentials: true
+    });
+  }
+
+  reject(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}/reject`, {}, {
+      withCredentials: true
+    });
+  }
+
+  approve(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}/approve`, {}, {
+      withCredentials: true
+    });
+  }
+  
+}
