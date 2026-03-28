62a731471cc5952c73681fb808998fecec19c961
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-03-28 12:37:19 -0300

    feat(order): implements order-list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { ProductListComponent } from './features/product/pages/list/product-list
 import { ProductCreateComponent } from './features/product/pages/create/product-create.component';
 import { ProductEditComponent } from './features/product/pages/edit/product-edit.component';
 import { ProductService } from './features/product/services/product.service';
+import { OrderListComponent } from './features/order/pages/list/order-list.component';
 
 export const routes: Routes = [
 
@@ -91,6 +92,12 @@ export const routes: Routes = [
             )
         },
         component: ProductEditComponent
+    },
+
+    {
+        path: 'orders',
+        canActivate: [authGuard],
+        component: OrderListComponent
     },
 
     {
--- src/app/features/order/services/order.service.spec.ts
+++ src/app/features/order/services/order.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { OrderService } from './order.service';
 
@@ -6,7 +7,9 @@ describe('OrderService', () => {
   let service: OrderService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
     service = TestBed.inject(OrderService);
   });
 
